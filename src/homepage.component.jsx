87fb2c2d7783f4a8ab862db42e929ef4a36ad2d6
Ankit Bhalla <ankit.bhalla@cars24.com>
2022-05-07 23:56:12 +0530

    Fixed styling issues

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import './homepage.styles.scss'
+
 const HomePage = () => {
     return (
         <div className="homepage">
             <div className="directory-menu">
-            <div className="menu-item">
+                <div className="menu-item">
                     <div className="content">
                         <h1 className="title">HATS</h1>
                         <span className="title">SHOP NOW</span>
@@ -16,14 +18,12 @@ const HomePage = () => {
                         <span className="title">SHOP NOW</span>
                     </div>
                 </div>
-
                 <div className="menu-item">
                     <div className="content">
                         <h1 className="title">JACKETS 1</h1>
                         <span className="title">SHOP NOW</span>
                     </div>
                 </div>
-
                 <div className="menu-item">
                     <div className="content">
                         <h1 className="title">JACKETS 2</h1>
