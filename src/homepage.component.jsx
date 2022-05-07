a428e846f60cec75a863bdc55f8ca970e6a5b30e
Ankit Bhalla <ankit.bhalla@cars24.com>
2022-05-07 23:52:34 +0530

    Created React application and added styling scss in code
    first commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+            <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="title">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JACKETS</h1>
+                        <span className="title">SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JACKETS 1</h1>
+                        <span className="title">SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JACKETS 2</h1>
+                        <span className="title">SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JACKETS 3</h1>
+                        <span className="title">SHOP NOW</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
